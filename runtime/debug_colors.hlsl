d18b1c6546734d37157e077fae60c18dc7e3c40e
David Detweiler <daviddetweiler1@gmail.com>
2021-11-29 11:31:23 -0800

    Implement naive phong shading

--- runtime/debug_colors.hlsl
+++ runtime/debug_colors.hlsl
@@ -6,10 +6,10 @@ cbuffer matrices : register(b0)
 	row_major float4x4 projection;
 };
 
-colored_vertex main(uint id : SV_VertexID, float3 position : POSITION, float3 normal : NORMAL)
+colored_vertex main(float3 position : POSITION, float3 normal : NORMAL)
 {
 	colored_vertex vertex;
 	vertex.position = mul(float4(position, 1.0f), mul(view, projection));
-	vertex.color = abs(normal);
+	vertex.color = dot(normal, float3(0.0f, 1.0f, 0.0f)) * 0.8f.xxx;
 	return vertex;
 }
